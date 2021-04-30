e493cc7841595b5067f0553e8facc52dafc31d9e
Dimitris Ioannou <43514376+dimitrisioan@users.noreply.github.com>
2021-04-30 21:31:08 +0300

    correct commit

---
+++ app/src/main/java/com/univ/ubitrack/GetStartedActivity.java
@@ -0,0 +1,15 @@
+package com.univ.ubitrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GetStartedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_started);
+    }
+}
+
