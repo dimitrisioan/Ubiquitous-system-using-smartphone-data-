9eeebc61cbe9760420428c772057873acd12f314
Evaggelos Soulintzis <esoulintzhs@gmail.com>
2021-05-14 22:31:44 +0300

    Add more API calls

--- app/src/main/java/com/univ/ubitrack/ThingsBoard.java
+++ app/src/main/java/com/univ/ubitrack/ThingsBoard.java
@@ -2,7 +2,6 @@ package com.univ.ubitrack;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.text.PrecomputedText;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -10,7 +9,9 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.android.gms.dynamic.IFragmentWrapper;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -23,9 +24,11 @@ public class ThingsBoard {
     private static final String AUTH_TOKEN_KEY = "AUTH_TOKEN_KEY";
     private static final String DEVICE_ID = "DEVICE_ID";
     private static final String CREDENTIALS_ID = "CREDENTIALS_ID";
+    private static final String LAST_ADDED_DEVICE_NAME = "LAST_ADDED_DEVICE_NAME";
     private String authToken;
     private String deviceId;
     private String credentialsId;
+    private String lastAddedDeviceName;
     private SharedPreferences sharedPreferences;
     private Context context;
     private String username = "omada7@ceid.upatras.gr";
@@ -39,6 +42,7 @@ public class ThingsBoard {
         authToken = sharedPreferences.getString(AUTH_TOKEN_KEY, "NO_KEY");
         deviceId = sharedPreferences.getString(DEVICE_ID, "NO_ID");
         credentialsId = sharedPreferences.getString(CREDENTIALS_ID, "NO_ID");
+        lastAddedDeviceName = sharedPreferences.getString(LAST_ADDED_DEVICE_NAME, "NO_NAME");
         obtainThingsBoardToken();
     }
 
@@ -89,30 +93,21 @@ public class ThingsBoard {
         VolleyController.getInstance(context).addToQueue(jsonObjReq);
     }
 
-    public void addNewDevice(){
-        String addDeviceURL = baseURL + "api/device";
-
-        String device_name = "Participant_7_1";
-        String device_type = "Participant";
+    public void getLastAddedDevice(int teamNumber){
+        String getLastAddedDeviceURL = baseURL + "api/tenant/devices?textSearch=Participant_" + teamNumber + "&sortProperty=name&sortOrder=desc&pageSize=10&page=0";
 
         JSONObject jsonBody = new JSONObject();
-        try {
-            jsonBody.put("name", device_name);
-            jsonBody.put("type", device_type);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
 
         String auth = sharedPreferences.getString("AUTH_TOKEN_KEY", "No key");
-        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST, addDeviceURL, jsonBody,
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, getLastAddedDeviceURL, jsonBody,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            String device_id = response.getJSONObject("id").getString("id");
-                            sharedPreferences.edit().putString(DEVICE_ID, device_id).apply();
+                            String device = response.getJSONArray("data").getJSONObject(0).getString("name");
+                            sharedPreferences.edit().putString(LAST_ADDED_DEVICE_NAME, device).apply();
                             if (MainActivity.debugging == 1)
-                                Log.i("DEVICE", "Device Added with ID -> " + device_id);
+                                Log.i("DEVICE", "Last added device name -> " + device);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -172,51 +167,13 @@ public class ThingsBoard {
         VolleyController.getInstance(context).addToQueue(jsonObjReq);
     }
 
-    public void checkIfDeviceExists() {
-        String checkDeviceURL = baseURL + "api/tenant/devices?deviceName=Participant_7_1";
-        String device_name = "Participant_7_1";
+    public void getDeviceIdFromDeviceName(String deviceName) {
+        String checkDeviceURL = baseURL + "api/tenant/devices?deviceName=";
 
         JSONObject jsonBody = new JSONObject();
 
         String auth = sharedPreferences.getString("AUTH_TOKEN_KEY", "No key");
-        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, checkDeviceURL, jsonBody,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        if (MainActivity.debugging == 1)
-                            Log.i("DEVICE", response.toString());
-                        statusCode = 400;
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d(TAG, error.toString());
-                        if (error.networkResponse.statusCode == 404)
-                            statusCode = 404;
-                        Toast.makeText(context, "Connection to ThingsBoard Failed", Toast.LENGTH_SHORT).show();
-                    }
-                }) {
-            @Override
-            public Map<String, String> getHeaders() {
-                HashMap<String, String> headers = new HashMap<>();
-                headers.put("Content-Type", "application/json");
-                headers.put("Accept", "application/json");
-                headers.put("X-Authorization", "Bearer " + auth);
-                return headers;
-            }
-        };
-        VolleyController.getInstance(context).addToQueue(jsonObjReq);
-    }
-
-    public void getDeviceId() {
-        String checkDeviceURL = baseURL + "api/tenant/devices?deviceName=Participant_7_1";
-        String device_name = "Participant_7_0";
-
-        JSONObject jsonBody = new JSONObject();
-
-        String auth = sharedPreferences.getString("AUTH_TOKEN_KEY", "No key");
-        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, checkDeviceURL, jsonBody,
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, checkDeviceURL + deviceName, jsonBody,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
@@ -249,9 +206,8 @@ public class ThingsBoard {
         VolleyController.getInstance(context).addToQueue(jsonObjReq);
     }
 
-    public void getDeviceCredentials() {
+    public void getDeviceCredentials(String deviceID) {
         String checkDeviceURL = baseURL + "api/device/";
-        String deviceID = "9685c4e0-b429-11eb-b9f4-cd7797153d94";
 
         JSONObject jsonBody = new JSONObject();
 
@@ -268,6 +224,47 @@ public class ThingsBoard {
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, error.toString());
+                        Toast.makeText(context, "Connection to ThingsBoard Failed", Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/json");
+                headers.put("Accept", "application/json");
+                headers.put("X-Authorization", "Bearer " + auth);
+                return headers;
+            }
+        };
+        VolleyController.getInstance(context).addToQueue(jsonObjReq);
+    }
+
+    public void addDeviceAttributes(String userAge, String gender, int team) {
+        String credentials = sharedPreferences.getString(CREDENTIALS_ID, "No Id");
+        String addDeviceAttributesURL = baseURL + "api/v1/" + credentials + "/attributes";
+
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("user_age", userAge);
+            jsonBody.put("user_sex", gender);
+            jsonBody.put("recruiting_team", team);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        String auth = sharedPreferences.getString("AUTH_TOKEN_KEY", "No key");
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST, addDeviceAttributesURL , jsonBody,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        if (MainActivity.debugging == 1)
+                            Log.i("DEVICE", "Attributes Added");
                     }
                 },
                 new Response.ErrorListener() {
